aa1e9118abfa7bc18fd20489964b4fa5d7aec974
Laurent G. OSSBSS AMIS UCC <27735554+LaGuer@users.noreply.github.com>
2019-03-30 19:10:48 -0700

    Create test_nb.py

---
+++ test_nb.py
@@ -0,0 +1,13 @@
+import subprocess
+import tempfile
+
+
+def _exec_notebook(path):
+    with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
+        args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
+                "--ExecutePreprocessor.timeout=1000",
+                "--output", fout.name, path]
+        subprocess.check_call(args)
+
+def test():
+    _exec_notebook('Julia%20Jupiter-Notebook%20Constants%20in%20Cosmology.ipynb')
