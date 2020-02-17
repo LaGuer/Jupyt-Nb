d8181a2ceae64d26cc70d1fc6d75d3eba8e5e059
Laurent G. OSSBSS AMIS UCC <user3@amisolution.net>
2020-02-17 07:20:37 +0100

    Create run_notebooks.py

---
+++ run_notebooks.py
@@ -0,0 +1,17 @@
+from runipy.notebook_runner import NotebookRunner
+#from nbformat import write
+#from nbformat import read
+from IPython.nbformat.current import write
+from IPython.nbformat.current import read
+from glob import glob
+
+files = glob("./[01]*ipynb")
+
+for file in files:
+    print("Processing file {}".format(file))
+    notebook = read(open(file), 'json')
+    r = NotebookRunner(notebook)
+    r.run_notebook(skip_exceptions=True)
+#    nbformat.current import write
+    write(r.nb, open("_runipy/"+file, 'w'), 'json')
+    print("Done file {}".format(file))
